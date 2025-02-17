2efda1919636c1d30d5515e2bdfd489cb3691eaa
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2025-02-17 03:18:40 +0100

    Update AntiCheatEvents.kt

--- grim-mod/src/main/kotlin/gg/norisk/verbrecher/utils/AntiCheatEvents.kt
+++ grim-mod/src/main/kotlin/gg/norisk/verbrecher/utils/AntiCheatEvents.kt
@@ -7,6 +7,7 @@ object AntiCheatEvents {
     val flagEvent = Event.syncAsync<FlagEvent>(false)
     val grimReloadEvent = Event.syncAsync<GrimReloadEvent>(false)
     val grimJoinEvent = Event.syncAsync<GrimJoinEvent>(false)
+    val grimQuitEvent = Event.syncAsync<GrimQuitEvent>(false)
     val commandExecuteEvent = Event.syncAsync<CommandExecuteEvent>(false)
     val completePredictionEvent = Event.syncAsync<CompletePredictionEvent>(false)
 }
